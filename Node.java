9c3b145e9256f070c1e47a925fad506682623773
mitrinh <32150483+mitrinh@users.noreply.github.com>
2018-02-08 00:30:49 -0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * @author tvnguyen7
+ */
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+    
+    public Node (T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+    
+    public void setNext (Node<T> next) {
+        this.next = next;
+    }
+    
+    public void setData (T data) {
+        this.data = data;
+    }
+    
+    public Node<T> getNext () {
+        return next;
+    }
+    
+    public T getData () {
+        return data;
+    }
+    
+    public String toString () {
+        return data.toString();
+    }
+}
